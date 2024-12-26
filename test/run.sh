d7287a8e276ab2c1a05311633cdcd99811d7ab50
chen <suchen@megaease.com>
2024-12-26 17:26:13 +0800

    fix dockerfile

--- test/run.sh
+++ test/run.sh
@@ -2,8 +2,7 @@ set -e
 
 echo "Starting the project..."
 
-vllm serve ./Qwen2.5-3B-Instruct --dtype auto --port 8000
+vllm serve ./Qwen2.5-3B-Instruct --dtype auto --port 8000 
 
 # Run python as a subprocess of the shell. So that in container, the python process will not be the process with PID 1.
 # You can easily kill the python process with kill command (`ps -eaf`) to restart the container.
-python3.11 -u ./test/main.py
